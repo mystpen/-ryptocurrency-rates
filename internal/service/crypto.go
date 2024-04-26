76da9d0e8c2d0ce1e765d8a11eebb6132074ee54
dokshiye <okshieva@gmail.com>
2024-04-26 17:02:00 +0500

    feat: implementation for interval update data from Api

--- internal/service/crypto.go
+++ internal/service/crypto.go
@@ -1,17 +1,53 @@
 package service
 
+import (
+	"errors"
+	"log"
+	"time"
+
+	"github.com/mystpen/cryptocurrency-rates/config"
+	"github.com/mystpen/cryptocurrency-rates/internal/model"
+	"github.com/mystpen/cryptocurrency-rates/internal/repository/api"
+)
+
 type ApiClient interface {
-	GetInfo() error
-	UpdateInfo() error
-	GetInfoByName() error
+	GetInfo() (*[]model.CryptoCoin, error)
+	UpdateInfo(*[]model.CryptoCoin) error
+	GetInfoByName(name string) (*model.CryptoCoin, error)
 }
 
 type Service struct {
-	apiClient  ApiClient
+	apiClient ApiClient
 }
 
-func NewService(apiClient ApiClient) *Service{
+func NewService(apiClient ApiClient) *Service {
 	return &Service{
 		apiClient: apiClient,
 	}
+}
+
+func (s *Service) IntervalUpdate() {
+	for {
+		info, err := s.apiClient.GetInfo()
+		if err != nil {
+			if !errors.Is(err, api.ErrNoResponce) { //TODO: change err, maybe add chan
+				log.Println(err)
+			} else {
+				log.Println(err)
+			}
+			
+		}
+
+		s.apiClient.UpdateInfo(info)
+
+		time.Sleep(config.Config.Interval)
+	}
+}
+
+func (s *Service) Init() {
+	go s.IntervalUpdate()
+}
+
+func (s *Service) GetInfoByName(name string) (*model.CryptoCoin, error){
+	return s.apiClient.GetInfoByName(name)
 }
